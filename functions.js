1a1defe163b3c83a150bbbc244f2153e1ded8da5
Szilard <153904683+szilAIrd@users.noreply.github.com>
2025-05-25 18:48:21 +0200

    Add remove book, set read status, add book features. Some fixes are still necessary to show the library content and the added books.

--- functions.js
+++ functions.js
@@ -10,57 +10,145 @@ function Book(title, author, year, read) {
     this.year = year;
     this.id = crypto.randomUUID()
     this.read = read;
-    this.setRead = function()
-        {this.read = true}
+    let id = this.id
+    // this.setReadStatus = function(id){
+    //     const bookRead = document.querySelector(`.book-card[data-id=${id}] .read`);
+    //     // this.read = true
+    //         // const bookRead = getElementById
+    //         if (this.read==true){
+                
+    //             this.read = false
+    //             bookRead.textContent = 'Not read';
+
+    //         }
+    //         else {
+    //             bookRead.textContent = 'Read'; 
+    //             this.read = true;
+    //         }
+    //     }
+        // () => {this.read == true ? `Book was read? ${this.read}` : `Book was read? ${!this.read}`;  }
+
+    
+    this.displayBook = function (){
+        const library = document.getElementById('library')
+    
+    
+        const bookCard = document.createElement('div');
+        bookCard.classList.add('book-card');
+        bookCard.dataset.id = this.id;
+        
+    
+        const bookTitle = document.createElement('div');
+        bookTitle.classList.add('title');
+        bookTitle.textContent = this.title
+        bookCard.appendChild(bookTitle)
+    
+    
+        const bookAuthor = document.createElement('div');
+        bookAuthor.classList.add('author');
+        bookAuthor.textContent = this.author;
+        bookCard.appendChild(bookAuthor)
+    
+
+        const bookYear = document.createElement('div');
+        bookYear.classList.add('year');
+        bookYear.textContent = this.year;
+        bookCard.appendChild(bookYear)
+    
+
+        const bookRead = document.createElement('div')
+        bookRead.classList.add('read')
+        bookRead.textContent = this.read;
+        bookCard.appendChild(bookRead);
+    
+
+        const bookActions = document.createElement('div')
+        bookActions.classList.add('actions')
+
+        const bookActionRead = document.createElement('div')
+        bookActionRead.classList.add('read-button')
+        bookActionRead.textContent = 'Set read';
+        bookActionRead.addEventListener('click',this.setReadStatus)
+        bookActions.appendChild(bookActionRead)
+
+        const bookActionDelete = document.createElement('div')
+        bookActionDelete.classList.add('delete-button')
+        bookActionDelete.textContent = 'Delete';
+        bookActionDelete.addEventListener('click',removeBook)
+
+        bookActions.appendChild(bookActionDelete)
+        bookCard.appendChild(bookActions)
+    
+        library.appendChild(bookCard)
+    }
 }
 
 let library = [];
 
-function addBookToLibrary(title, author,year,read){
-    let book = new Book(title, author, year, read)
-    return library.push(book)
-}
-
-addBookToLibrary('The Hobbit 1','Tolkien', 1971, false)
-addBookToLibrary('The Hobbit 2','Tolkien', 1972, false)
-addBookToLibrary('The Hobbit 3','Tolkien', 1973, false)
-
-function displayLibrary(){
-    const library = document.getElementById('library')
-    const bookCard = document.createElement('div');
-    bookCard.classList.add('book-card');
+Book.prototype.setReadStatus = function(){
     
-
-    const bookTitle = document.createElement('div');
-    bookTitle.classList.add('title');
-    bookCard.appendChild(bookTitle)
-
-    const bookAuthor = document.createElement('div');
-    bookAuthor.classList.add('author');
-    bookCard.appendChild(bookAuthor)
-
-    const bookYear = document.createElement('div');
-    bookYear.classList.add('year');
-    bookCard.appendChild(bookYear)
-
-    const bookRead = document.createElement('div')
-    bookRead.classList.add('read')
-    bookCard.appendChild(bookRead)
-
-    const bookActions = document.createElement('div')
-    bookActions.classList.add('actions')
-    const bookActionRead = document.createElement('div')
-    bookActionRead.classList.add('read-button')
-    bookActions.appendChild(bookActionRead)
-    const bookActionDelete = document.createElement('div')
-    bookActionDelete.classList.add('delete-button')
-    bookActions.appendChild(bookActionDelete)
-    bookCard.appendChild(bookActions)
-
-
-
-
-    library.appendChild(bookCard)
+    this.read = !this.read;
+    let currentElem = this;
+    let bookRead = this.parentElement.previousElementSibling;
+    bookRead.textContent = this.read;
+    this.textContent = ( this.read === true ? 'Set not read' : 'Set read')
+    
+    let bookId = currentElem.parentElement.parentElement.dataset.id;
+    
+    let index = library.findIndex(book => book.id === bookId)
+    library[index].read=this.read
 
 }
 
+function removeBook(){
+    let currentElem = this;
+    let bookCard = currentElem.parentElement.parentElement
+    let bookId = bookCard.dataset.id;
+
+    let index = library.findIndex(book => book.id=== bookId)
+    if (index !== -1) {
+        library.splice(index, 1); // removes the item in-place
+      }
+
+    bookCard.remove()
+
+    // const bookId = document.querySelector(`[data-user=]`)
+}
+
+function addBookToLibrary(event,title, author,year,read=false, ){
+    if (arguments[0]!='') {
+        title = title;
+        author = author;
+        year = year;
+        read=read
+    }
+    else{
+        const newBook = document.getElementById('add-book-form')
+        title = newBook.children[0].textContent.value;
+        author = newBook.children[1].textContent.value;
+        year = newBook.children[3].textContent.value;
+    }
+    // const addBook = document.getElementById('add-book-btn')
+    event.preventDefault()
+    let book = new Book(title, author, year, read)
+    
+    library.push(book)
+    return displayBook()
+}
+
+// addBookToLibrary('The Hobbit 1','Tolkien', 1971, false)
+// addBookToLibrary('The Hobbit 2','Tolkien', 1972, false)
+// addBookToLibrary('The Hobbit 3','Tolkien', 1973, false)
+
+
+const addBook = document.getElementById('submit-book')
+addBook.addEventListener('click', addBookToLibrary, false)
+
+
+function displayLibrary(library){
+   library.forEach(element => {element.displayBook()});
+}
+
+
+
+displayLibrary(library)
